6f3ca0d16c0afdb6d14ed89c8782f760f3c488f3
ASHUASHFA <ashfaqashu2006@gmail.com>
2026-07-08 15:40:41 +0530

    upate dashboard02

--- dashboard.py
+++ dashboard.py
@@ -10,11 +10,25 @@ def home():
     cursor = conn.cursor()
 
     cursor.execute("SELECT * FROM logs")
-
     logs = cursor.fetchall()
+
+    cursor.execute("SELECT COUNT(*) FROM logs")
+    total = cursor.fetchone()[0]
+
+    cursor.execute("SELECT COUNT(*) FROM logs WHERE action='BLOCKED'")
+    blocked = cursor.fetchone()[0]
+
+    cursor.execute("SELECT COUNT(*) FROM logs WHERE action='ALLOWED'")
+    allowed = cursor.fetchone()[0]
 
     conn.close()
 
-    return render_template("index.html", logs=logs)
+    return render_template(
+        "index.html",
+        logs=logs,
+        total=total,
+        blocked=blocked,
+        allowed=allowed
+    )
 
 app.run(debug=True)
